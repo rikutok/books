7851cad18d87424679ac912eb6cc45ba49d1d95d
kohak R <ninja8rrrr@gmail.com>
2017-01-08 06:43:38 +0000

    git commit first

--- app.rb
+++ app.rb
@@ -1,3 +1,20 @@
 require 'bundler/setup'
 Bundler.require
 require 'sinatra/reloader' if development?
+
+require 'net/http'
+require 'uri'
+require 'json'
+
+post '/search' do
+    keyword = params[:keyword];
+    uri = URI.parse('https://www.googleapis.com/books/v1/volumes?q=' + keyword + '&country=JP')
+    json = Net::HTTP.get(uri)
+    result = JSON.parse(json)
+    @items = result['items']
+    erb :index2 
+end
+
+get '/' do
+    erb :index
+end
